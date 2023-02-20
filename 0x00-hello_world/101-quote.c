5940b59b7701ad6eceaf356e9e22b959f435c18e
ddpeculiar2023 <ddpeculiar@gmail.com>
2023-02-19 17:39:00 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A progaram that prints a line without using printf or put function
+ * Return: 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
