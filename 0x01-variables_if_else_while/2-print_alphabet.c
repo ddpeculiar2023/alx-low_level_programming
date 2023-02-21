b473d2ba213704be83d2a40f660844d6a64a9243
ddpeculiar2023 <ddpeculiar@gmail.com>
2023-02-21 10:12:58 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - A program that prints the alphabet in lowercase
+  *
+  *Return: Always 0
+  */
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
